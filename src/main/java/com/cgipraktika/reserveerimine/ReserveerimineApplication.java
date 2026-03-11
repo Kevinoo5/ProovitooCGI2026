7aa08c6294fe7e0f7ea3aaaec36fadc43f348605
Kevinoo5 <kevinnoormets@gmail.com>
2026-03-11 13:46:29 +0200

    Generate project files

---
+++ src/main/java/com/cgipraktika/reserveerimine/ReserveerimineApplication.java
@@ -0,0 +1,13 @@
+package com.cgipraktika.reserveerimine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveerimineApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ReserveerimineApplication.class, args);
+    }
+
+}
